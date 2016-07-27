db1cc59cde11fde35c372372bad6f5b27580be04
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-27 08:05:12 -0500

    initial commit

---
+++ routes/login.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+
+
+router.get('/', function(req, res, next) {
+  res.json(req.isAuthenticated());
+});
+
+
+router.post('/',
+  passport.authenticate('local', {
+    successRedirect: '/views/success.html',
+    failureRedirect: '/views/failure.html'
+  })
+);
+
+
+module.exports = router;
